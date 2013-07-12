b493e7d402c0cfd6a9ea5e9ad773402a0a9f23d5
scott.adams <scott.adams@thecurrencycloud.com>
2013-07-12 15:47:12 +0100

    Able to validate missing and extra parameters

--- lib/sinatra/chiro/middleware.rb
+++ lib/sinatra/chiro/middleware.rb
@@ -1,23 +1,74 @@
 module Sinatra
   module Chiro
     class Middleware
+
       def initialize(app, opts={})
         @app = app
         @documentation = opts[:documentation] || Sinatra::Chiro.class_variable_get('@@documentation')
       end
-
       def call(env)
         query_string = env['QUERY_STRING']
         show_help = query_string.include?('help')
-        return @app.call(env) unless show_help
+        url = env['PATH_INFO']
 
-        path = env['PATH_INFO']
-        docs = @documentation.select { |d| d.path.include?(path)}.flatten.first
-        raise "Path #{path} doesn't have any docs" unless docs
+        return @app.call(env) if url.include?('favicon.ico')
 
-        response_body = [docs.to_json]
+        if show_help
+          return generate_help(url)
+        end
+
+        response = validate(url, env)
+        return response if response
+
+        @app.call(env)
+      end
+
+      def generate_help(url)
+        endpoint = @documentation.select { |d| d.path.include?(url) }.flatten.first
+        raise "Path #{url} doesn't have any docs" unless endpoint
+
+        response_body = [endpoint.to_json]
 
         [200, {'Content-Type' => 'application/json'}, response_body]
+      end
+
+      def validate(url, env)
+
+        query_string = env['QUERY_STRING']
+        query_hash = Rack::Utils.parse_nested_query("#{query_string}")
+
+       errors = []
+=begin
+        query_hash.each do |k, v|
+          unless k == 'people'
+            @message = "#{k} parameter must be a String" if k.type != String
+          else
+            @message = "people parameter must be an Array" if k.type != Array
+          end
+        end
+=end
+
+        ['language', 'year', 'people', 'greeting'].each do |parameter|
+          errors << "must include a #{parameter} parameter" if query_hash[parameter] == nil
+        end
+
+        query_hash.each do|k,v|
+          errors << "#{k} is not a valid parameter" if (k!="language" and k!="people" and k!="greeting" and k!="year")
+        end
+
+        [200, {'Content-Type' => 'text/plain'}, [errors.join("\n")]] if !errors.empty?
+
+
+        #find the endpoint using the 'url'
+        ## if there is no endpoint, just return
+        #get all the query string parameters from the endpoint
+        #pull query string parameters from 'env'
+
+        #check that there are no extra parameters
+        #check that all required parameters are present
+        #check that parameters is of the right type
+
+
       end
     end
   end
--- test/server.rb
+++ test/server.rb
@@ -4,16 +4,20 @@ class HelloApp < Sinatra::Base
   register Sinatra::Reloader
   register Sinatra::Chiro
 
-  #http://localhost:9292/hi/hello?people[]=roger&people[]=john
+  #http://localhost:9292/hi?people[]=richard&people[]=scott&people[]=roger&greeting=hi
   endpoint "Says hi to the caller" do
-    named_param(:greeting, 'How you want to be greeted')
-    named_param(:people, 'Who you want to greet', :type => Array[String])
+    query_param(:greeting, 'How you want to be greeted')
+    query_param(:year, 'The year to greet people with', :type => Fixnum, :optional => false)
+    query_param(:people, 'Who you want to greet', :type => Array[String], :optional => false)
     query_param(:language, 'Language to be greeted in', :type => /^en$|^af-za$|^fr$/)
     #payload(:contact_id, '')
 
-    get '/hi/:greeting' do
-      greeting = params[:greeting] || 'Hello'
-      language = params[:language] || 'en'
+
+
+    get '/hi' do
+      greeting = params[:greeting] || 'Hello'   # gets greeting parameter from url
+      language = params[:language] || 'en'      # gets language parameter from url
+      year = params[:year].to_i                 # gets year parameter from url
 
       greetee = if params[:people]
                   params[:people].join(", ")
@@ -23,10 +27,13 @@ class HelloApp < Sinatra::Base
                     when 'fr' then 'monde'
                     else 'world'
                   end
-                end
+                end                              # if no value param for people given in url, use language given for world
 
-      "#{greeting} #{greetee}!"
+      "#{greeting} #{greetee}! Happy new year for #{year}"
     end
+
+
+
 
     returns "e.g. Hello world!"
   end
