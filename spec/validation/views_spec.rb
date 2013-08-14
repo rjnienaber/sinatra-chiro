d9dacb0d5d2e96c4bc3d696ba903bfb6756236a4
scott.adams <scott.adams@thecurrencycloud.com>
2013-08-14 16:55:34 +0100

    fixed issue caused by SERVER_APP constant

--- spec/validation/views_spec.rb
+++ spec/validation/views_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 ru_file = File.dirname(__FILE__) + '/../example_apps/user_views/config.ru'
-SERVER_APP = Rack::Builder.parse_file(ru_file).first
+SERVER_APP2 = Rack::Builder.parse_file(ru_file).first
 
 
 p settings.erb_file
 
 describe 'Classic Style Application' do
   def app
-    SERVER_APP
+    SERVER_APP2
   end
 
   it 'displays new erb file for routes' do
